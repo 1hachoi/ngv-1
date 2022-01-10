f6446557ff1e8cff693fd170d71a963acd2dcd38
ecourse <smkang@codenuri.co.kr>
2022-01-10 16:51:19 +0900

    Add files via upload

---
+++ DAY1/if.c
@@ -0,0 +1,35 @@
+// 23 page
+#include <stdio.h> 
+
+// 모든 C프로그램은 main 함수의 1번째 줄부터 순차적으로 실행
+
+int main()
+{
+    int score = 60;    // 이 값을 변경하면서 보세요
+    
+    if ( score >= 70 ) {
+        printf("pass\n");
+    }
+    else
+    {
+        printf("fail\n");
+    }
+    printf("이 문장은 항상 실행\n");
+    return 0;
+}
+
+// github.com/codenuri/ngv  에 매일 매일의 수업소스 올려 놓겠습니다.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ DAY1/input.c
@@ -0,0 +1,34 @@
+// 23 page
+#include <stdio.h> 
+
+// 30 page 입력
+int main()
+{
+    int score1 = 0;
+    int score2 = 0;
+
+    scanf("%d", &score1);
+    scanf("%d", &score2);
+
+    // 한줄로 할수도 있지만 보통 위 처럼 2줄로 많이 합니다.
+    
+    printf("%d\n", score1 + score2);
+
+    return 0;
+}
+
+// github.com/codenuri/ngv  에 매일 매일의 수업소스 올려 놓겠습니다.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
