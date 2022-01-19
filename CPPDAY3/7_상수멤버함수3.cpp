6b8536a5572737f808576a42f54728e93b28200f
ecourse <smkang@codenuri.co.kr>
2022-01-19 13:56:54 +0900

    Add files via upload

--- CPPDAY3/7_상수멤버함수3.cpp
+++ CPPDAY3/7_상수멤버함수3.cpp
@@ -1,18 +1,25 @@
+// 상수 멤버 함수는 선택이 아닙니다... 필수 문법 입니다.
+// 객체의 상태를 변경하지 않은 모든 멤버 함수(getter) 는 반드시 
+// 상수 멤버함수가 되어야 합니다. - 122page 제일 아래 부분
+
 class Rect
 {
+	
 	int x, y, w, h;
 public:
 	Rect(int a, int b, int c, int d) : x(a), y(b), w(c), h(d) {}
-
-	int getArea() { return w * h; }
+	int getArea() const { return w * h; }
 };
-void foo(Rect r)
+//void foo(Rect r) // call by value : 복사본의 오버헤드가 있다.
+
+void foo(const Rect& r) // const & 가 좋다.
 {
 	int n = r.getArea();
 }
+
 int main()
 {
-	Rect r(1, 1, 10, 10); // ?
-	int n = r.getArea();
+	Rect r(1, 1, 10, 10); // 상수 객체 아님
+	int n = r.getArea();  // ok. 
 	foo(r);
 }
---
+++ CPPDAY3/7_상수멤버함수4.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <vector>
+
+class Test
+{
+	int data;
+public:
+	// 1. 동일 이름의 상수 멤버 함수의 비상수 멤버를 동시에 제공가능
+	void foo()       { std::cout << "foo()"       << std::endl; } // 1
+	void foo() const { std::cout << "foo() const" << std::endl; } // 2
+};
+int main()
+{
+	// 결과 예측해 보세요
+	Test t1;        
+	t1.foo();		// 1.   없으면 2번 호출
+
+	const Test t2;
+	t2.foo();		// 2.   없다면 error
+
+	std::vector<int>       v1 = { 1,2,3 };
+	const std::vector<int> v2 = { 1,2,3 };
+
+	// 아래 2줄 예상해 보세요
+	v1[0] = 10; // v1.operator[](0) 입니다.
+	v2[0] = 10; // error. v2.operator[](0) 입니다.
+	int n = v2[0]; // ok
+
+	// 즉, 상수   vector 의 "operator[]" 함수의 반환값과
+	// 즉, 비상수 vector 의 "operator[]" 함수의 반환값의 형태가 다릅니다.
+
+}
+
+// 2:05 분에 이어집니다.
