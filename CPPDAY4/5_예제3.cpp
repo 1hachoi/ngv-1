12961e09d30e34f3ef18fdca177f448f7166659d
ecourse <smkang@codenuri.co.kr>
2022-01-20 11:33:37 +0900

    Add files via upload

--- CPPDAY4/5_예제3.cpp
+++ CPPDAY4/5_예제3.cpp
@@ -7,6 +7,20 @@
 // 핵심 4. 파생 클래스가 재정의(override) 하게 되는 모든 멤버 함수는
 //		   반드시 "가상함수(virtual)"로 해야 한다.
 
+// 핵심 5. OCP ( Open Close Principle ) - 160 page
+// 
+//     기능 확장에는 열려 있고(Open, 나중에 class/module 등이 추가되어도)
+//     코드 수정에는 닫혀 있어야(Close, 기존 코드에는 수정이 없어야 한다)
+//     는 원칙(Principle)
+
+// Open Close Principle(OCP)라고 합니다.
+
+// 다형성(가상함수 기반 코드)은 OCP를 만족하는 아주 좋은 코딩입니다.
+
+
+
+
+
 class Shape 
 {
 public:
@@ -33,6 +47,14 @@ public:
 	void Draw() { std::cout << "Draw Circle" << std::endl; }
 };
 
+// 세월이 지나서 미래에 새로운 도형을 추가 했습니다.
+class Triangle : public Shape
+{
+public:
+	void Draw() { std::cout << "Draw Triangle" << std::endl; }
+};
+
+
 int main()
 {
 	std::vector<Shape*> v;
@@ -51,7 +73,16 @@ int main()
 		else if (cmd == 9)
 		{
 			for (auto p : v) 
-				p->Draw();   
+				p->Draw();  // 이 표현식은, 항상 같은 함수를 호출하나요 ?
+							// ==> 상황에 따라 다르게 동작합니다.
+						    // ==> "다형성(Polymorphism)" 이라고 불리는 개념입니다.
+							// 
+			// 객체지향 언어의 3대 특징 : 캡슐화, 상속, 다형성(Polymorphism)
+
+			// 다형성은 왜?? 중요 한가요 ?
+			// 캡슐화 : 외부에 잘못된 사용으로 객체의 상태 보호(b.gear = -1 가 안되게)
+			// 상속   : 기존타입을 확장해서 새로운 타입설계
+			// 다형성 : 
 		}
 	}
 }
