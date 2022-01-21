d0ec10e08881e10c7be802bd9cac36d79c1734b1
ecourse <smkang@codenuri.co.kr>
2022-01-21 09:43:33 +0900

    Add files via upload

--- CPPDAY5/2_연산자재정의4.cpp
+++ CPPDAY5/2_연산자재정의4.cpp
@@ -1,12 +1,23 @@
 // 2_연산자재정의4 - 187 page
-#include <iostream>
+//#include <iostream>
+#include <cstdio>
+
+class ostream
+{
+public:
+	void operator<<(int n)    { printf("%d", n); }
+	void operator<<(double d) { printf("%f", d); }
+};
+ostream cout; // 전역적으로 만들어진 객체(변수)
 
 int main()
 {
 	int n = 10;
 	double d = 3.4;
 
-	std::cout << n;
-	std::cout << d;
+	std::cout << n; // cout.operator<<(int)
+	std::cout << d; // cout.operator<<(double)
 }
+
+
 
