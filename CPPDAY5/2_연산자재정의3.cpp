407b4948d343c1748113fa4686315dfea47759e0
ecourse <smkang@codenuri.co.kr>
2022-01-21 09:40:19 +0900

    Add files via upload

--- CPPDAY5/2_연산자재정의3.cpp
+++ CPPDAY5/2_연산자재정의3.cpp
@@ -6,7 +6,7 @@ class Point
 	int x, y;
 public:
 	Point(int a, int b) : x(a), y(b) {}
-	/*
+	
 	// 이항연산자를 멤버 함수로 구현하면 인자는 한개가 됩니다.
 	Point operator+(const Point& p)
 	{
@@ -14,7 +14,7 @@ public:
 		Point temp(x + p.x, y + p.y);
 		return temp;
 	}
-	*/
+	
 	friend Point operator+(const Point& p1, const Point& p2);
 };
 // 일반 함수로 구현한 operator+
@@ -33,5 +33,23 @@ int main()
 
 	Point p3 = p1 + p2;  // 1. p1.operator+(p2)   를 검색
 						 // 2. operator+(p1, p2)  를 검색
+	// 멤버가 좋은 가요 ? 멤버가 아닌 함수가 좋은가요 ?
+	// ==> 개발자 성향에 따라 다릅니다.
+	// ==> 그런데, 멤버로 만들수 없을때가 있습니다
+	int n = 10;
+	Point p5 = p1 + p2; // p1.operator+(Point)
+	Point p6 = p1 + n;  // p1.operator+(n)
+	Point p7 = n  + p1; // n.operator+(Point) 로 할수는 없다.
+						// operator+(int, Point)로 해야 한다.
+	// 즉, 
+	// "primitive type + user type" 은 멤버가 아닌 함수로 해야 한다.
+
+	// boiler plate : 유사한 코드를 계속 만드는 것을 부르는 용어
 }
+
+
+
+
+
+
 
---
+++ CPPDAY5/2_연산자재정의4.cpp
@@ -0,0 +1,12 @@
+// 2_연산자재정의4 - 187 page
+#include <iostream>
+
+int main()
+{
+	int n = 10;
+	double d = 3.4;
+
+	std::cout << n;
+	std::cout << d;
+}
+
